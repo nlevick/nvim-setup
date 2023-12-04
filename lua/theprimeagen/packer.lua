298ea501796fbfacc8221dc056d536115a4af617
Nathan Levick <nlevick@indicalab.com>
2023-12-04 08:40:05 -0700

    Cleanup mappings, trailing spaces

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -25,10 +25,10 @@ return require('packer').startup(function(use)
 
   use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
   use ('nvim-treesitter/playground')
-  use ('theprimeagen/harpoon') 
-  use ('mbbill/undotree') 
-  use ('tpope/vim-fugitive') 
-  
+  use ('theprimeagen/harpoon')
+  use ('mbbill/undotree')
+  use ('tpope/vim-fugitive')
+
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v3.x',
