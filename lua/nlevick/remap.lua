21c304114cb7cfd199db156e2f281370fca5ed9a
nlevick <nlevick@indicalab.com>
2024-03-12 15:00:52 -0600

    Cleanup remaps

--- lua/nlevick/remap.lua
+++ lua/nlevick/remap.lua
@@ -17,15 +17,12 @@ vim.keymap.set("n", "N", "Nzzzv")
 --[[ vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
 end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
 ]]
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- copy to clipboard 
+-- copy to clipboard
 vim.keymap.set("n", "<leader>Y", [["+y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
@@ -34,21 +31,31 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-b>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>m", [[:mksession! ms<CR>]])
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- vim.keymap.set("n", "<leader>bo", [[:up|%bd|e#|bd#<CR>]]);
-vim.keymap.set("n", "<leader>bo", [[:%bd|e#<CR>]]);
-vim.keymap.set("n", "<leader>bq", [[:bp|bw#<CR>]]);
+-- tabs
+vim.keymap.set("n", "<leader>1", "1gt")
+vim.keymap.set("n", "<leader>2", "2gt")
+vim.keymap.set("n", "<leader>3", "3gt")
+vim.keymap.set("n", "<leader>4", "4gt")
+vim.keymap.set("n", "<leader>5", "5gt")
+vim.keymap.set("n", "<leader>6", "6gt")
+vim.keymap.set("n", "<leader>7", "7gt")
+vim.keymap.set("n", "<leader>8", "8gt")
+vim.keymap.set("n", "<leader>9", "9gt")
+vim.keymap.set("n", "<leader>0", "[[:tablast<CR>]]")
+
+vim.keymap.set("n", "<leader>q", [[:bp<bar>sp<bar>bn<bar>bd<CR>]])
+vim.keymap.set("n", "<leader>o", [[:up|%bd|e#|bd#<CR>]]);
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
