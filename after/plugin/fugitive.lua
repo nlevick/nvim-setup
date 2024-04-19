d143abb2c588e979206ecd68d700e7e6642f9270
nlevick <nlevick@indicalab.com>
2024-04-19 14:48:50 -0600

    Remap fugitive keys

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<leader>gs", function()
+vim.keymap.set("n", "<leader>gg", function()
     vim.cmd(":tab Git")
 end)
--- vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
--- vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
