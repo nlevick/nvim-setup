926748e8fdc6b61b108f8c875b080194f1e051f1
nlevick <nlevick@indicalab.com>
2024-05-17 14:28:36 -0600

    Rework telescope, add live grep args

--- lua/nlevick/plugins/telescope.lua
+++ lua/nlevick/plugins/telescope.lua
@@ -3,15 +3,37 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-live-grep-args.nvim" },
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "TelescopeResults",
+			callback = function(ctx)
+				vim.api.nvim_buf_call(ctx.buf, function()
+					vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+					vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+				end)
+			end,
+		})
+
+		local function filenameFirst(_, path)
+			local tail = vim.fs.basename(path)
+			local parent = vim.fs.dirname(path)
+			if parent == "." then
+				return tail
+			end
+			return string.format("%s\t\t%s", tail, parent)
+		end
+
 		telescope.setup({
 			defaults = {
-				path_display = { "smart" },
+				-- path_display = { shorten = { len = 3, exclude = { -1 } } },
+				path_display = filenameFirst,
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -25,17 +47,34 @@ return {
 					},
 				},
 			},
+			extensions = {
+				live_grep_args = {
+					auto_quoting = false, -- enable/disable auto-quoting
+					mappings = { -- extend mappings
+						i = {
+							["<C-e>"] = require("telescope-live-grep-args.actions").quote_prompt({
+								postfix = " -Thtml",
+							}),
+						},
+					},
+				},
+			},
 		})
 
-		--   telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
+		telescope.load_extension("live_grep_args")
 		local builtin = require("telescope.builtin")
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		-- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set(
+			"n",
+			"<leader>fc",
+			require("telescope-live-grep-args.shortcuts").grep_word_under_cursor,
+			{ desc = "Find string under cursor in cwd" }
+		)
+		keymap.set("v", "<leader>fv", require("telescope-live-grep-args.shortcuts").grep_visual_selection)
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 
 		-- Browse directories
@@ -114,8 +153,8 @@ return {
 		keymap.set(
 			"n",
 			"<leader>fa",
-			[[<Cmd>lua require'telescope.builtin'.live_grep()<CR>]],
-			{ noremap = true, silent = true, desc = "Live grep" }
+			require("telescope").extensions.live_grep_args.live_grep_args,
+			{ noremap = true, silent = true, desc = "Live grep in cwd" }
 		)
 		-- open buffer: content
 		keymap.set(
