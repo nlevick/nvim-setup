32de5cf48ba2b4146b079fb9523b457e2936e29f
nlevick <nlevick@indicalab.com>
2024-06-03 16:02:45 -0600

    Add recent files to alpha dash

--- lua/nlevick/plugins/alpha.lua
+++ lua/nlevick/plugins/alpha.lua
@@ -21,10 +21,11 @@ return {
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
 			dashboard.button("SPC yy", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fa", "  > Find Word", "<cmd>Telescope live_grep_args<CR>"),
+			dashboard.button("SPC fr", "⧗  > Recent files", "<cmd>Telescope oldfiles<CR>"),
 			dashboard.button("SPC mr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
 		-- Send config to alpha
