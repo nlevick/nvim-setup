b60179ef3f385ab4b3dad035ad5259efa9ae297f
nlevick <nlevick@indicalab.com>
2024-09-12 17:00:25 -0600

    Add fugitive split shortcut

--- lua/nlevick/plugins/fugitive.lua
+++ lua/nlevick/plugins/fugitive.lua
@@ -6,6 +6,9 @@ return {
 		keymap.set("n", "<leader>gg", function()
 			vim.cmd(":tab Git")
 		end)
+		keymap.set("n", "<leader>gx", function()
+			vim.cmd("vert Git")
+		end, { desc = "git fugitive" })
 		keymap.set("n", "gf", "<cmd>diffget //2<CR>", { desc = "Take left changes" })
 		keymap.set("n", "gj", "<cmd>diffget //3<CR>", { desc = "Take right changes" })
 	end,
