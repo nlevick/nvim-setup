2da9460a607717465688b2233839bd1dcdb0c97d
nlevick <nlevick@indicalab.com>
2024-10-09 12:13:22 -0600

    Update typescript tool shortcuts

--- lua/nlevick/plugins/lsp/typescript-tools.lua
+++ lua/nlevick/plugins/lsp/typescript-tools.lua
@@ -4,8 +4,14 @@ return {
 	event = "VeryLazy",
 	config = function()
 		local keymap = vim.keymap
-		keymap.set("n", "<leader>i", "<cmd>TSToolsSortImports<CR>", { desc = "TSTools sort imports" })
-		keymap.set("n", "<leader>ir", "<cmd>TSToolsRemoveUnusedImports<CR>", { desc = "TSTools remove unused imports" })
+		keymap.set("n", "<leader>is", "<cmd>TSToolsSortImports<CR><cmd>w<CR>", { desc = "Sort imports" })
+		keymap.set(
+			"n",
+			"<leader>ir",
+			"<cmd>TSToolsRemoveUnusedImports<CR><cmd>w<CR>",
+			{ desc = "Remove unused imports" }
+		)
+		keymap.set("n", "<leader>ia", "<cmd>TSToolsAddMissingImports<CR><cmd>w<CR>", { desc = "Add missing imports" })
 
 		local tsTools = require("typescript-tools")
 
