dc82cd0c7b34829dbf0114ba39b7b42b3cbd7587
nlevick <nlevick@indicalab.com>
2025-03-11 09:27:20 -0600

    Add markdown preview package

---
+++ lua/nlevick/plugins/markdownpreview.lua
@@ -0,0 +1,19 @@
+return {
+	-- For `plugins/markview.lua` users.
+	-- "OXY2DEV/markview.nvim",
+	-- lazy = false,
+	-- "iamcco/markdown-preview.nvim",
+	-- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- ft = { "markdown" },
+	-- build = function()
+	-- 	vim.fn["mkdp#util#install"]()
+	-- end,
+
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
+}
