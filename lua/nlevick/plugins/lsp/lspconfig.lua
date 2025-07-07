72efe511bb20d26de17736ca38815a191c1a8dec
nlevick <nlevick@indicalab.com>
2025-07-07 10:04:12 -0600

    Update packages post nvim v11

--- lua/nlevick/plugins/lsp/lspconfig.lua
+++ lua/nlevick/plugins/lsp/lspconfig.lua
@@ -110,28 +110,13 @@ return {
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		mason_lspconfig.setup_handlers({
+		mason_lspconfig.setup({
 			-- default handler for installed servers
 			function(server_name)
 				lspconfig[server_name].setup({
 					capabilities = capabilities,
 				})
 			end,
-			-- ["svelte"] = function()
-			-- 	-- configure svelte server
-			-- 	lspconfig["svelte"].setup({
-			-- 		capabilities = capabilities,
-			-- 		on_attach = function(client, bufnr)
-			-- 			vim.api.nvim_create_autocmd("BufWritePost", {
-			-- 				pattern = { "*.js", "*.ts" },
-			-- 				callback = function(ctx)
-			-- 					-- Here use ctx.match instead of ctx.file
-			-- 					client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-			-- 				end,
-			-- 			})
-			-- 		end,
-			-- 	})
-			-- end,
 			["graphql"] = function()
 				-- configure graphql language server
 				lspconfig["graphql"].setup({
@@ -139,33 +124,6 @@ return {
 					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
 				})
 			end,
-			-- ["emmet_ls"] = function()
-			-- 	-- configure emmet language server
-			-- 	lspconfig["emmet_ls"].setup({
-			-- 		capabilities = capabilities,
-			-- 		filetypes = {
-			-- 			"html",
-			-- 			"typescriptreact",
-			-- 			"javascriptreact",
-			-- 			"css",
-			-- 			"sass",
-			-- 			"scss",
-			-- 			"less",
-			-- 			"svelte",
-			-- 		},
-			-- 	})
-			-- end,
-			-- ["omnisharp"] = function()
-			-- 	lspconfig["omnisharp"].setup({
-			-- 		capabilities = capabilities,
-			-- 		filetypes = {
-			-- 			"cs",
-			-- 		},
-			-- 		enable_roslyn_analyzers = true,
-			-- 		-- organize_imports_on_format = true,
-			-- 		enable_import_completion = true,
-			-- 	})
-			-- end,
 			["ts_ls"] = function() end,
 			["lua_ls"] = function()
 				-- configure lua server (with special settings)
