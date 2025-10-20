4d2b6391c8a3ee86654f738b071b2b43a22786ff
nlevick <nlevick@indicalab.com>
2025-10-20 13:53:40 -0600

    Open all search results in quick list cmd

--- lua/nlevick/plugins/telescope.lua
+++ lua/nlevick/plugins/telescope.lua
@@ -40,6 +40,7 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-a>"] = actions.send_to_qflist + actions.open_qflist,
 						["<C-x>"] = "select_vertical",
 						["<C-o>"] = function(prompt_bufnr) -- open file under cursor in tab, resume
 							require("telescope.actions").select_tab(prompt_bufnr)
