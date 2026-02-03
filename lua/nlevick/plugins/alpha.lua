ed350f3828d9f8796f82451a5d13479516f89a34
nlevick <nlevick@indicalab.com>
2026-02-03 06:38:39 -0700

    Splash page styling

--- lua/nlevick/plugins/alpha.lua
+++ lua/nlevick/plugins/alpha.lua
@@ -5,6 +5,16 @@ return {
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
 
+		local version = vim.version()
+		local nvim_version = string.format("v%d.%d.%d", version.major, version.minor, version.patch)
+
+		-- Define the total width of the ASCII art (based on the longest line)
+		local total_width = 53
+
+		-- local padding = total_width - #nvim_version
+		local padding = math.floor((total_width - #nvim_version) / 2)
+		local version_line = string.rep(" ", padding) .. nvim_version
+
 		-- Set header
 		dashboard.section.header.val = {
 			"                                                     ",
@@ -15,6 +25,7 @@ return {
 			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
 			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
 			"                                                     ",
+			version_line,
 		}
 
 		-- Set menu
@@ -24,8 +35,15 @@ return {
 			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fa", "  > Find Word", "<cmd>Telescope live_grep_args<CR>"),
 			dashboard.button("SPC fr", "⧗  > Recent files", "<cmd>Telescope oldfiles<CR>"),
-			dashboard.button("SPC mr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("SPC mr", "󰁯  > Restore Session For Current Directory", "<cmd>AutoSession restore<CR>"),
 			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+		}
+
+		dashboard.section.footer.val = {
+			"",
+			"sabbe saṅkhārā aniccā",
+			"sabbe saṅkhārā dukkhā",
+			"sabbe dhammā anattā",
 		}
 
 		-- Send config to alpha
